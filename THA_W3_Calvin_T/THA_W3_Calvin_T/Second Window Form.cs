fd99d5f052818a0b788cbd5ebd6863a60351346b
calvct <112923033+calvct@users.noreply.github.com>
2023-03-17 20:32:25 +0700

    Add files via upload

--- THA_W3_Calvin_T/THA_W3_Calvin_T/Second Window Form.cs
+++ THA_W3_Calvin_T/THA_W3_Calvin_T/Second Window Form.cs
@@ -41,38 +41,6 @@ namespace THA_W3_Calvin_T
 
         private void btn_magic_Click(object sender, EventArgs e)
         {
-            if (rb_red.Checked)
-            {
-                mainform.BackColor = Color.IndianRed;
-            }
-            else if (rb_green.Checked)
-            {
-                mainform.BackColor = Color.GreenYellow;
-            }
-            else if (rb_blue.Checked)
-            {
-                mainform.BackColor = Color.AliceBlue;
-            }
-            else if (rb_pink.Checked)
-            {
-                mainform.BackColor = Color.Pink;
-            }
-            else if (rb_yellow.Checked)
-            {
-                mainform.BackColor = Color.LightYellow;
-            }
-            if (rb_navy.Checked)
-            {
-                mainform.ForeColor = Color.Navy;
-            }
-            else if (rb_purple.Checked)
-            {
-                mainform.ForeColor = Color.Purple;
-            }
-            else if (rb_grey.Checked)
-            {
-                mainform.ForeColor = Color.Gray;
-            }
             if (!rb_blue.Checked && !rb_green.Checked && !rb_red.Checked && !rb_pink.Checked && !rb_yellow.Checked && !rb_navy.Checked && !rb_purple.Checked && !rb_grey.Checked)
             {
                 MessageBox.Show("Choose background and text color", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
@@ -85,6 +53,43 @@ namespace THA_W3_Calvin_T
             {
                 MessageBox.Show("Choose text color", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
+            else
+            {
+                if (rb_red.Checked)
+                {
+                    mainform.BackColor = Color.IndianRed;
+                }
+                else if (rb_green.Checked)
+                {
+                    mainform.BackColor = Color.GreenYellow;
+                }
+                else if (rb_blue.Checked)
+                {
+                    mainform.BackColor = Color.AliceBlue;
+                }
+                else if (rb_pink.Checked)
+                {
+                    mainform.BackColor = Color.Pink;
+                }
+                else if (rb_yellow.Checked)
+                {
+                    mainform.BackColor = Color.LightYellow;
+                }
+                if (rb_navy.Checked)
+                {
+                    mainform.ForeColor = Color.Navy;
+                }
+                else if (rb_purple.Checked)
+                {
+                    mainform.ForeColor = Color.Purple;
+                }
+                else if (rb_grey.Checked)
+                {
+                    mainform.ForeColor = Color.Gray;
+                }
+            }
+            
+            
         }
     }
 }
